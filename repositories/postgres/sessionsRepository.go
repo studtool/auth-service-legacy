6fa5ace81e6cdec357d4e10abdef473dc4513b03
denis96z <zinoviev.denis@yandex.ru>
2019-04-03 21:49:22 +0300

    session schema added

--- repositories/postgres/sessionsRepository.go
+++ repositories/postgres/sessionsRepository.go
@@ -1,8 +1,11 @@
 package postgres
 
 import (
+	"auth-service/beans"
+	"auth-service/config"
 	"auth-service/errs"
 	"auth-service/models"
+	"auth-service/utils"
 )
 
 type SessionsRepository struct {
@@ -15,6 +18,31 @@ func NewSessionsRepository(conn *Connection) *SessionsRepository {
 		conn:             conn,
 		notAuthorizedErr: errs.NewNotAuthorizedError("session not found"),
 	}
+}
+
+func (r *SessionsRepository) Init() error {
+	err := utils.Retry(func(n int) error {
+		if n > 0 {
+			beans.Logger.Infof("opening storage: retry #%d", n)
+		}
+		return r.conn.db.Ping()
+	}, config.StorageConnNumRet, config.StorageConnRetItv)
+	if err != nil {
+		return err
+	}
+
+	const query = `
+        CREATE TABLE IF NOT EXISTS session (
+            user_id  TEXT
+              CONSTRAINT session_user_id_pk PRIMARY KEY,
+            auth_token TEXT NOT NULL,
+            refresh_token TEXT NOT NULL,
+            expire_time TIMESTAMPTZ NOT NULL
+        );
+	`
+
+	_, err = r.conn.db.Exec(query)
+	return err
 }
 
 func (r *SessionsRepository) AddSession(session *models.Session) *errs.Error {
