1e7cc4da0680af088a2099f91c46cfc082ab398e
denis96z <zinoviev.denis@yandex.ru>
2019-04-12 23:23:30 +0300

    refactoring

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go.uber.org/dig"
 	"os"
 	"os/signal"
@@ -32,82 +31,80 @@ func main() {
 	panicOnErr(c.Provide(api.NewServer))
 
 	if config.RepositoriesEnabled.Value() {
-		_ = c.Invoke(func(conn *postgres.Connection) {
+		panicOnErr(c.Invoke(func(conn *postgres.Connection) {
 			if err := conn.Open(); err != nil {
 				beans.Logger.Fatal(err)
 			} else {
 				beans.Logger.Info("storage connection: open")
 			}
-		})
+		}))
 		defer func() {
-			_ = c.Invoke(func(conn *postgres.Connection) {
+			panicOnErr(c.Invoke(func(conn *postgres.Connection) {
 				if err := conn.Close(); err != nil {
 					beans.Logger.Fatal(err)
 				} else {
 					beans.Logger.Info("connection to storage: closed")
 				}
-			})
+			}))
 		}()
 	}
 
 	if config.QueuesEnabled.Value() {
-		_ = c.Invoke(func(q *mq.MQ) {
+		panicOnErr(c.Invoke(func(q *mq.MQ) {
 			if err := q.OpenConnection(); err != nil {
 				beans.Logger.Fatal(err)
 			} else {
 				beans.Logger.Info("message queue connection: open")
 			}
-		})
+		}))
 		defer func() {
-			_ = c.Invoke(func(q *mq.MQ) {
+			panicOnErr(c.Invoke(func(q *mq.MQ) {
 				if err := q.CloseConnection(); err != nil {
 					beans.Logger.Fatal(err)
 				} else {
 					beans.Logger.Info("message queue connection: closed")
 				}
-			})
+			}))
 		}()
 	}
 
 	ch := make(chan os.Signal)
 	signal.Notify(ch, os.Interrupt)
 
-	_ = c.Invoke(func(srv *api.Server) {
+	panicOnErr(c.Invoke(func(srv *api.Server) {
 		go func() {
 			if err := srv.Run(); err != nil {
 				beans.Logger.Fatal(err)
 				ch <- os.Interrupt
 			}
 		}()
-
-		beans.Logger.Info(fmt.Sprintf("server: started; [port: %s]", config.ServerPort.Value()))
-	})
+	}))
 	defer func() {
-		_ = c.Invoke(func(srv *api.Server) {
+		panicOnErr(c.Invoke(func(srv *api.Server) {
 			if err := srv.Shutdown(); err != nil {
 				beans.Logger.Fatal(err)
 			} else {
 				beans.Logger.Info("server: down")
 			}
-		})
+		}))
 	}()
 
 	if config.DiscoveryClientEnabled.Value() {
-		_ = c.Invoke(func(cl *discovery.Client) {
+		panicOnErr(c.Invoke(func(cl *discovery.Client) {
 			if err := cl.Register(); err != nil {
 				beans.Logger.Fatal(err)
 			} else {
 				beans.Logger.Info("discovery client connection: open")
 			}
-		})
+		}))
 		defer func() {
-			_ = c.Invoke(func(cl *discovery.Client) {
+			panicOnErr(c.Invoke(func(cl *discovery.Client) {
 				if err := cl.Unregister(); err != nil {
 					beans.Logger.Fatal(err)
 				} else {
 					beans.Logger.Info("discovery client connection: closed")
 				}
-			})
+			}))
 		}()
 	}
 
